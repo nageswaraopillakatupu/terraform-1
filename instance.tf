ca4defd22f868ad70d7733f74247625113e5400a
nageswaraopillakatupu <150110854+nageswaraopillakatupu@users.noreply.github.com>
2024-01-19 16:34:52 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -1,10 +1,53 @@
-provider "aws" {
-  access_key = "AKIA2UMQDC5HI6XERQXV"
-  secret_key = "eew0ynhIUnJ8jcNxkEvyRX5YDrCazfM46AFPDQs"
-  region = "us-east-1"
-}
+resource "harness_platform_service" "example" {
+  identifier  = "identifier"
+  name        = "name"
+  description = "test"
+  org_id      = "org_id"
+  project_id  = "project_id"
 
-resource "aws_instance" "example" {
-  ami = "ami-0279c3b3186e54acd"
-  instance_Type = "t2.micro"
+  ## SERVICE V2 UPDATE
+  ## We now take in a YAML that can define the service definition for a given Service
+  ## It isn't mandatory for Service creation 
+  ## It is mandatory for Service use in a pipeline
+
+  yaml = <<-EOT
+                service:
+                  name: name
+                  identifier: identifier
+                  serviceDefinition:
+                    spec:
+                      manifests:
+                        - manifest:
+                            identifier: manifest1
+                            type: K8sManifest
+                            spec:
+                              store:
+                                type: Github
+                                spec:
+                                  connectorRef: <+input>
+                                  gitFetchType: Branch
+                                  paths:
+                                    - files1
+                                  repoName: <+input>
+                                  branch: master
+                              skipResourceVersioning: false
+                      configFiles:
+                        - configFile:
+                            identifier: configFile1
+                            spec:
+                              store:
+                                type: Harness
+                                spec:
+                                  files:
+                                    - <+org.description>
+                      variables:
+                        - name: var1
+                          type: String
+                          value: val1
+                        - name: var2
+                          type: String
+                          value: val2
+                    type: Kubernetes
+                  gitOpsEnabled: false
+              EOT
 }
